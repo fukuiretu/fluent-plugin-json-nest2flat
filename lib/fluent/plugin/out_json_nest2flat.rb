71eec19e8201fc8a8f685ecd7999f83014bd8565
FukuiReTu <fukui_retu@FukuiReTu-no-MacBook-Air.local>
2014-01-11 21:54:04 +0900

    add parameter [ignore_item_keys]

--- lib/fluent/plugin/out_json_nest2flat.rb
+++ lib/fluent/plugin/out_json_nest2flat.rb
@@ -1,11 +1,11 @@
 module Fluent
     class JsonNestToFlatOutput < Fluent::Output
-
         Fluent::Plugin.register_output('json_nest2flat', self)
 
         config_param :tag, :string, :default => nil
         config_param :add_tag_prefix, :string, :default => nil
         config_param :json_keys, :string, :default => nil
+        config_param :ignore_item_keys, :string, :default => nil
 
         def configure(conf)
             super
@@ -27,6 +27,16 @@ module Fluent
             end
 
             @json_keys = @json_keys.split(",")
+
+            @ignore_item_keys = conf['ignore_item_keys']
+
+            if !@ignore_item_keys.nil?
+                begin
+                    @ignore_item_keys = JSON.parse(@ignore_item_keys)
+                rescue => e
+                   raise Fluent::ConfigError, "ignore_item_keys is illegal! ignore_item_keys=#{@ignore_item_keys}"
+                end
+            end
         end
 
         def emit(tag, es, chain)
@@ -49,15 +59,28 @@ module Fluent
         # @return [Hash] ネストされたJSONをフラットな構造に変換したレコード
         private
         def _convert_record(old_record)
-            new_record = Hash.new([])
+            new_record = {}
             json_keys_exist_count = 0
 
             old_record.each { |old_record_k, old_record_v|
-                if (@json_keys.include?(old_record_k))
+                if @json_keys.include?(old_record_k)
                     json_data = old_record[old_record_k]
-                    JSON.parse(json_data).each { |json_k, json_v|
-                        new_record[json_k] = json_v
-                    }
+                    begin
+                        JSON.parse(json_data).each { |json_k, json_v|
+                            if @ignore_item_keys.include?(old_record_k)
+                                # 無視するキーに該当
+                                ignore_items = @ignore_item_keys[old_record_k]
+                                if ignore_items.include?(json_k)
+                                    # 無視するアイテムに該当するのでハッシュに含まない
+                                    next
+                                end
+                            end
+                            new_record[json_k] = json_v
+                        }
+                    rescue => e
+                        $log.error "json_data is parse error.json_data=#{json_data}", :error=>e.to_s
+                        raise
+                    end
 
                     json_keys_exist_count += 1
                 else
@@ -71,6 +94,5 @@ module Fluent
 
             return new_record
         end
-
     end
 end
